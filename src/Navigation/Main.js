3fe248db53c2ec45c4a705855605618ffb3e2a7c
mainamnghia99 <mai.nam.nghia.bunbu@gmail.com>
2021-03-19 13:52:25 +0900

    home screen

--- src/Navigation/Main.js
+++ src/Navigation/Main.js
@@ -3,16 +3,8 @@ import { Image, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StatusBar } from "expo-status-bar";
-import Icon from 'react-native-vector-icons/FontAwesome5'
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Icon from "react-native-vector-icons/FontAwesome5";
+import HomePage from "../Screens/HomePage/HomePage";
 
 function RoommateScreen() {
   return (
@@ -51,32 +43,34 @@ const Tab = createBottomTabNavigator();
 export default function MainNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          if (route.name === 'Home') {
-            iconName = 'home';
-          } else if (route.name === 'Roommate') {
-            iconName = 'user-friends';
-          } else if (route.name === 'Favourite') {
-            iconName = 'heart';
-          } else if (route.name === 'Message') {
-            iconName = 'comment-dots';
-          } else if (route.name === 'Settings') {
-            iconName = 'user-cog';
-          }
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Roommate") {
+              iconName = "user-friends";
+            } else if (route.name === "Favourite") {
+              iconName = "heart";
+            } else if (route.name === "Message") {
+              iconName = "comment-dots";
+            } else if (route.name === "Settings") {
+              iconName = "user-cog";
+            }
 
-          // You can return any component that you like here!
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: '#00CDDA',
-        inactiveTintColor: 'gray',
-      }}
-      initialRouteName={HomeScreen}>
-        <Tab.Screen name="Home" component={HomeScreen} />
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#00CDDA",
+          inactiveTintColor: "gray",
+        }}
+        initialRouteName={HomePage}
+      >
+        <Tab.Screen name="Home" component={HomePage} />
         <Tab.Screen name="Roommate" component={RoommateScreen} />
         <Tab.Screen name="Favourite" component={FavouriteScreen} />
         <Tab.Screen name="Message" component={MessageScreen} />
---
+++ src/Screens/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+const HomePage = () => {
+  return (
+    <ScrollView>
+      <Text>HomePage</Text>
+    </ScrollView>
+  );
+};
+
+export default HomePage;
